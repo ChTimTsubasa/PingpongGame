4c86f51923c628d7dbfb8309a2cf571a5b04a990
Tianyi Chen <tianyic1@andrew.cmu.edu>
2017-11-04 19:58:23 -0400

    add channel sessions

--- src/pingpong/pingponggame/consumers.py
+++ src/pingpong/pingponggame/consumers.py
@@ -4,6 +4,8 @@ from channels import Group
 from channels.auth import channel_session_user, channel_session_user_from_http
 
 redis_conn = redis.Redis("localhost", 6379)
+
+@channel_session_user_from_http
 # Connected to websocket.connect
 def ws_add(message):
     # Accept the connection
@@ -11,12 +13,14 @@ def ws_add(message):
     # Add to the chat group
     Group("chat").add(message.reply_channel)
 
+@channel_session_user
 # Connected to websocket.receive
 def ws_message(message):
     Group("chat").send({
         "text": "[user] %s" % message.content['text'],
     })
 
+@channel_session_user
 # Connected to websocket.disconnect
 def ws_disconnect(message):
     print("some one leaves")
--- src/pingpong/pingponggame/models.py
+++ src/pingpong/pingponggame/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from django.template.loader import render_to_string
+
+from channels import Group
+
 # Player model
 class Player(models.Model):
 	user = models.OneToOneField(User)
