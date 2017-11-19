966efa8907c8c5768a2feb177bc63bdd1345f699
Tianyi Chen <tianyic1@andrew.cmu.edu>
2017-11-18 22:32:06 -0500

    replace the app.js and game_room.js

--- src/pingpong/pingponggame/static/js/app.js
+++ src/pingpong/pingponggame/static/js/app.js
@@ -687,82 +687,74 @@ function sendPosition() {
 }
 
 console.log('exmaple.js file game_room part!!!!!!!!!!');
-
 function handle(message) {
-    console.log(message)
-    if (message.TYPE == 'STATE') {
-        if (message.state == 'ready') {
-            $('#win_but').prop('disabled', false);
-        } else {
-            $('#win_but').prop('disabled', true);
-        }
-    } else if (message.TYPE == "GAME") {
-        alert('winner is ' + message.winner);
-    }
+  // console.log(message)
+  console.log(message.TYPE)
+  switch (message.TYPE) {
+      case 'STATE':
+          if (message.STATE == 'start') {
+              $('#win_but').prop('disabled', false);
+          } else {
+              $('#win_but').prop('disabled', true);
+          }
+          break;
+      case 'PAD':
+          console.log(message)
+          break;
+      case 'BALL':
+          console.log(message)
+          break;
+  }
 }
 
 //send requets to get player information inside room up-to-date
 function sendRequest() {
-    gameid = $('#game').val();
-    $.getJSON("getPlayersInfo/"+gameid, function(data) {
-        updatePlayerInfo(data);
-    });
+  gameid = $('#game').val();
+  $.getJSON("getPlayersInfo/"+gameid, function(data) {
+      updatePlayerInfo(data);
+  });
 }
 
 //Update player information in web page
 function updatePlayerInfo(data) {
-    players = data.players;
-    console.log(players);
+  players = data.players;
+  // console.log(players);
 
-    $('#creator').html(players[0].html);
-    $('#opponent').html(players[1].html);
+  $('#creator').html(players[0].html);
+  $('#opponent').html(players[1].html);
 }
 
-
 $(document).ready(function () {
-    console.log('game room ready')
+  var game_id = $('#game').val()
+  var socket = new WebSocket('ws://' + window.location.host + '/game/' + game_id);
 
-    setTimeout(function() {
-        console.log('timeout done');
-        var socket = new WebSocket('ws://' + window.location.host);
-        sendRequest();
-        window.setInterval(sendRequest, 1000);
+  sendRequest();
+  window.setInterval(sendRequest, 1000);
+  var d = new Date();
 
-        window.setInterval(sendPosition, 300);
-        // window.setInterval(findBrick, 300);
+  socket.onmessage = function(e) {
+      var data = jQuery.parseJSON(e.data)
+      handle(data);
+  }
 
+  socket.onopen = function() {
+  }
 
-        socket.onmessage = function(e) {
-            var data = jQuery.parseJSON(e.data)
-            handle(data);
-        }
+  socket.onclose = function() {
+      socket.close();
+  }
 
-        socket.onopen = function() {
-            // socket.send("hello world");
-        }
+  $('#win_but').prop('disabled', true);
 
-        socket.onclose = function() {
-            socket.close();
-        }
+  $('#win_but').click(function() {
+      console.log ("button clicked")
+      socket.send(JSON.stringify({
+          TYPE: "STATE",
+          state: 'ready',
+      }));
 
-        $('#win_but').prop('disabled', true);
-
-        $('#win_but').click(function() {
-            console.log ("button clicked")
-            socket.send(JSON.stringify({
-                TYPE: "GAME",
-                score: Math.floor(1 + Math.random() * 10)
-            }));
-
-            $('#win_but').prop('disabled', true);
-        })
-    }
-    , 1000);
-    
+      $('#win_but').prop('disabled', true);
+  })
 });
-
-  // $( document ).ready(function() { 
-  //   window.setInterval(sendPosition, 300);
-  // })
 
 });
