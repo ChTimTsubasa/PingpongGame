e52e345f3daaf271c9017bd3d032cffd076da651
Tianyi Chen <tianyic1@andrew.cmu.edu>
2017-11-28 21:15:56 -0500

    modify routing for static file

--- src/pingpong/pingpong/routing.py
+++ src/pingpong/pingpong/routing.py
@@ -1,4 +1,5 @@
 from channels.routing import route, route_class
+from channels.staticfiles import StaticFilesConsumer
 from pingponggame.consumers import GameServer
 from channels import include
 
@@ -6,7 +7,13 @@ game_routing = [
     route_class(GameServer, path = '^/(?P<room_id>[a-zA-Z0-9_]+)$')
 ]
 
+channel_routing = [
+    # Serve static files
+    route('http.request', StaticFilesConsumer()),
+]
+
 routing = [
     # You can use a string import path as the first argument as well.
     include(game_routing, path = r'^/game'),
+    include(channel_routing),
 ]
