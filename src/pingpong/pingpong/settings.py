4080e57e8f2cd5d47c399099d4c74c07b7a556a9
Tianyi Chen <tianyic1@andrew.cmu.edu>
2017-12-01 23:32:28 -0500

    use original database

--- src/pingpong/pingpong/settings.py
+++ src/pingpong/pingpong/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'pingpong.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': config.get('database','DATABASE_ENGINE'), # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pingpong_t',#config.get('database','DATABASE_NAME'),                     # Or path to database file if using sqlite3.
+        'NAME': config.get('database','DATABASE_NAME'),                     # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': config.get('database', 'DATABASE_USER'),
     }
